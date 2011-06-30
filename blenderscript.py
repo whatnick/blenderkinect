050cb5e74aba9b54b3ffad548c9c452ef983b499
whatnick <whatnickd@gmail.com>
2011-06-30 16:41:59 -0700

    Swappeed to higher res and more stable async mode of the Python API

--- blenderscript.py
+++ blenderscript.py
@@ -1,6 +1,6 @@
+#!/usr/bin/env python
 import sys
 
-sys.path.append("/home/dylan/Desktop/OpenKinect-libfreenect-116570b/c/python/")
 from freenect import *
 
 from Blender import *
@@ -9,19 +9,20 @@ import Blender, meshtools
 import numpy as np
 
 def drawmesh(mesh):
-	Blender.NMesh.PutRaw(mesh,"obj",1)
-	Blender.Redraw()
+    Blender.NMesh.PutRaw(mesh,"obj",1)
+    Blender.Redraw()
 
-def display(dev, data, timestamp):
-	data -= np.min(data.ravel())
-	mesh = Blender.NMesh.GetRaw()
-	for y in range(0,480,5):
-		for x in range(0,640,5):
-			zz = data[y,x]
-			if(zz<1000):
-				xx = (x-320)*(zz-10)*0.0021  #This 'calibration' copied using Zephod's windows software - because it 'just works'
-				yy = (y-240)*(zz-10)*0.0021
-				zz-=200
-				mesh.verts.append(NMesh.Vert(xx, yy, -zz))
-	drawmesh(mesh)
-runloop(depth_cb_np_factory(display), lambda *x: None)
+def display(data):
+    data -= np.min(data.ravel())
+    mesh = Blender.NMesh.GetRaw()
+    for y in range(0,480,5):
+        for x in range(0,640,5):
+            zz = data[y,x]
+            if(zz<1000):
+                xx = (x-320)*(zz-10)*0.0021 #This 'calibration' copied using Zephod's windows software - because it 'just works'
+                yy = (y-240)*(zz-10)*0.0021
+                zz-=200
+                mesh.verts.append(NMesh.Vert(xx, yy, -zz))
+    drawmesh(mesh)
+    
+display(sync_get_depth()[0])
